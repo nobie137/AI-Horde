edddf2cc3152a6a85a10c65a772e779a0664c848
Divided by Zer0 <mail@dbzer0.com>
2022-10-08 14:01:45 +0200

    oops

--- horde/countermeasures.py
+++ horde/countermeasures.py
@@ -3,7 +3,6 @@ from . import logger, args
 from .redis_ctrl import is_redis_up, get_ipaddr_db
 from datetime import timedelta
 
-
 r = None
 logger.init("IP Address Cache", status="Connecting")
 if is_redis_up():
@@ -38,12 +37,12 @@ def is_ip_safe(ipaddr):
 		probability = float(result.content)
 		if not result.ok:
 			if probability == int(os.getenv("IP_CHECKER_LC")):
-				is_safe = set_safe(True)
+				is_safe = set_safe(ipaddr,True)
 			else:
-				is_safe = set_safe(False)
+				is_safe = set_safe(ipaddr,False)
 				logger.error(f"An error occured while validating IP. Return Code: {result.text}")
 		else:
-			is_safe = set_safe(probability < safety_threshold)
+			is_safe = set_safe(ipaddr, probability < safety_threshold)
 	logger.debug(f"IP {ipaddr} has a probability of {probability}. Safe = {is_safe}")
 	return(is_safe)
 
